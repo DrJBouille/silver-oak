70d4bcc5da55945070b32d5c5f1cfe8616b6fea3
DrJBouille <jeanbaptiste0605@gmail.com>
2025-11-15 22:36:35 +0100

    move ClassesRegistry

---
+++ apps/silver-oak-core/src/main/java/com/api/silver_oak_core/registries/ClassesRegistry.java
@@ -0,0 +1,35 @@
+package com.api.silver_oak_core.registries;
+
+import com.api.silver_oak_core.model.classes.Classes;
+import com.api.silver_oak_core.model.classes.Warrior;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Supplier;
+
+@Service
+public class ClassesRegistry {
+  private static final Map<String, Supplier<Classes>> CLASSES = new HashMap<>();
+
+  static {
+    CLASSES.put("warrior", Warrior::new);
+  }
+
+  public Classes getClass(String name) throws RuntimeException {
+    Supplier<Classes> supplier = CLASSES.get(name);
+
+    if (supplier == null) throw new RuntimeException("Class not found: " + name);
+    return supplier.get().clone();
+  }
+
+  public Set<String> getNames() {
+    return CLASSES.keySet();
+  }
+
+  public List<Classes> getClasses() {
+    return CLASSES.values().stream().map(supplier -> supplier.get().clone()).toList();
+  }
+}
