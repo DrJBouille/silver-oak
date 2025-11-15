650886121213c078f412242ad1512e685201d71a
DrJBouille <jeanbaptiste0605@gmail.com>
2025-11-15 22:42:12 +0100

    create classes controller

--- apps/silver-oak-core/src/main/java/com/api/silver_oak_core/controllers/ClassesController.java
+++ apps/silver-oak-core/src/main/java/com/api/silver_oak_core/controllers/ClassesController.java
@@ -1,20 +1,32 @@
 package com.api.silver_oak_core.controllers;
 
-import com.api.silver_oak_core.model.classes.ClassesRegistry;
+import com.api.silver_oak_core.model.classes.Classes;
+import com.api.silver_oak_core.registries.ClassesRegistry;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Set;
 
 @RestController
 @RequestMapping("/api/classes")
+@RequiredArgsConstructor
 public class ClassesController {
+  private ClassesRegistry classesRegistry;
+
   @PreAuthorize("hasAuthority('USER')")
-  @GetMapping()
+  @GetMapping("/names")
   public ResponseEntity<Set<String>> getClassesRegistry() {
-    return ResponseEntity.ok(ClassesRegistry.getClasses());
+    return ResponseEntity.ok(classesRegistry.getNames());
+  }
+
+  @PreAuthorize("hasAuthority('USER')")
+  @GetMapping
+  ResponseEntity<List<Classes>> getClasses() {
+    return ResponseEntity.ok(classesRegistry.getClasses());
   }
 }
--- apps/silver-oak-core/src/main/java/com/api/silver_oak_core/model/classes/Classes.java
+++ apps/silver-oak-core/src/main/java/com/api/silver_oak_core/model/classes/Classes.java
@@ -1,23 +1,23 @@
 package com.api.silver_oak_core.model.classes;
 
 import com.api.silver_oak_core.model.weapons.WeaponType;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 import java.util.List;
 
+@RequiredArgsConstructor
+@Getter
 public abstract class Classes implements Cloneable {
-  private final String name;
-
-  protected Classes(String name) {
-    this.name = name;
-  }
-
+  public final String name;
   public abstract List<WeaponType> getUsableWeapons();
 
   @Override
-  public Classes clone() throws CloneNotSupportedException {
+  public Classes clone() {
     try {
       return (Classes) super.clone();
     } catch (CloneNotSupportedException e) {
-      throw new CloneNotSupportedException();
+      throw new AssertionError();
     }
   }
 }
